cde9248c0cf9e7a70c5f54632c48476c14f0d345
valentin fourny <fourny.valentin@gmail.com>
2022-06-16 20:37:25 +0200

    preprae functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,15 @@
 import admin from 'firebase-admin';
-import { auth } from 'firebase-functions';
+import functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 
-export const createFirestoreUser = auth.user().onCreate((user) => {
-  console.log(user);
-  admin.firestore().collection('users').doc(user.uid).set({
-    email: user.email,
+export const createFirestoreUser = functions.https.onCall((data, context) => {
+  console.log(data);
+  console.log(context);
+  admin.firestore().collection('users').doc(data.uid).set({
+    email: data.email,
+    username: data.username,
   });
 });
